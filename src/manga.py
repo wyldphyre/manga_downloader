9b0a3ba73b5f6f6ea175428a233d0404b6c6fcd0
Jiawei Li <jiawei.h.li@gmail.com>
2012-01-23 15:36:04 -0600

    manga.py cleanup

--- src/manga.py
+++ src/manga.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2010  
+# Copyright (C) 2010-2012
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,7 +22,6 @@ import os
 import sys
 
 ##########
-
 from parsers.thread import SiteParserThread
 from util import fixFormatting, isImageLibAvailable
 from xmlparser import MangaXmlParser
@@ -32,200 +31,197 @@ from outputManager.progressBarManager import progressBarManager
 VERSION = 'v0.8.8'
 
 siteDict = {
-		''  : '[mf]',
-		'1' : '[mf]',
-		'2' : '[ow]',
-		'3' : '[mr]'
-					}
+    '': '[mf]',
+    '1': '[mf]',
+    '2': '[ow]',
+    '3': '[mr]'
+}
 
 ##########
 
 class InvalidSite(Exception):
-	pass
+    pass
+
 
 def printLicenseInfo():
-		print( "\nProgram: Copyright (c) 2010. GPL v3 (http://www.gnu.org/licenses/gpl.html)." )
-		print( "Icon:      Copyright (c) 2006. GNU Free Document License v1.2 (Author:Kasuga)." )
-		print( "           http://ja.wikipedia.org/wiki/%E5%88%A9%E7%94%A8%E8%80%85:Kasuga\n" )
-		
+    print( "\nProgram: Copyright (c) 2010. GPL v3 (http://www.gnu.org/licenses/gpl.html)." )
+    print( "Icon:      Copyright (c) 2006. GNU Free Document License v1.2 (Author:Kasuga)." )
+    print( "           http://ja.wikipedia.org/wiki/%E5%88%A9%E7%94%A8%E8%80%85:Kasuga\n" )
+
 ##########
-		
+
 def main():
-	printLicenseInfo()
-	
-	# for easier parsing, adds free --help and --version
-	# optparse (v2.3-v2.7) was chosen over argparse (v2.7+) for compatibility (and relative similarity) reasons 
-	# and over getopt(v?) for additional functionality
-	parser = optparse.OptionParser(	usage='usage: %prog [options] <manga name>', 
-					version=('Manga Downloader %s' % VERSION)									)
-					
-	parser.set_defaults(	
-				all_chapters_FLAG = False,
-				auto = False,
-				conversion_FLAG = False,
-				convert_Directory = False,
-				device = 'Kindle 3',
-				downloadFormat = '.cbz', 
-				downloadPath = 'DEFAULT_VALUE', 
-				inputDir = None,
-				outputDir = 'DEFAULT_VALUE',
-				overwrite_FLAG = False,
-				verbose_FLAG = False,
-				timeLogging_FLAG = False,
-				maxChapterThreads = 3 														)
-				
-	parser.add_option(	'--all', 
-				action = 'store_true', 
-				dest = 'all_chapters_FLAG', 
-				help = 'Download all available chapters.'										)
-				
-	parser.add_option(	'-d', '--directory', 
-				dest = 'downloadPath', 
-				help = 'The destination download directory.  Defaults to the directory of the script.'					)
-				
-	parser.add_option(	'--overwrite', 
-				action = 'store_true', 
-				dest = 'overwrite_FLAG', 
-				help = 'Overwrites previous copies of downloaded chapters.'								)
+    printLicenseInfo()
 
-	parser.add_option(	'--verbose', 
-				action = 'store_true', 
-				dest = 'verbose_FLAG', 
-				help = 'Verbose Output.'								)
-				
-	parser.add_option(	'-x','--xml', 
-				dest = 'xmlfile_path', 
-				help = 'Parses the .xml file and downloads all chapters newer than the last chapter downloaded for the listed mangas.'	)
-	
-	parser.add_option(	'-c', '--convertFiles', 
-				action = 'store_true', 
-				dest = 'conversion_FLAG', 
-				help = 'Converts downloaded files to a Format/Size acceptable to the device specified by the --device parameter.'				)
+    # for easier parsing, adds free --help and --version
+    # optparse (v2.3-v2.7) was chosen over argparse (v2.7+) for compatibility (and relative similarity) reasons
+    # and over getopt(v?) for additional functionality
+    parser = optparse.OptionParser(usage='usage: %prog [options] <manga name>',
+        version=('Manga Downloader %s' % VERSION))
 
-	parser.add_option( 	'--device', 
-				dest = 'device', 
-				help = 'Specifies the conversion device. Omitting this option default to %default.'				)
-	
-	parser.add_option( 	'--convertDirectory', 
-				action = 'store_true', 
-				dest = 'convert_Directory', 
-				help = 'Converts the image files stored in the directory specified by --inputDirectory. Stores the converted images in the directory specified by --outputDirectory'	)
-	
-	parser.add_option( 	'--inputDirectory', 
-				dest = 'inputDir', 
-				help = 'The directory containing the images to convert when --convertDirectory is specified.'					)
-	
-	parser.add_option( 	'--outputDirectory', 
-				dest = 'outputDir', 
-				help = 'The directory to store the images when --convertDirectory is specified.'					)				
-											
-	parser.add_option(	'-z', '--zip', 
-				action = 'store_const', 
-				dest = 'downloadFormat', 
-				const = '.zip', 
-				help = 'Downloads using .zip compression.  Omitting this option defaults to %default.'					)
-	
-	parser.add_option(	'-t', '--threads', 
-				dest = 'maxChapterThreads', 
-				help = 'Limits the number of chapter threads to the value specified.'					)
-	
-	parser.add_option(	'--timeLogging', 
-				action = 'store_true', 
-				dest = 'timeLogging_FLAG', 
-				help = 'Output time logging.'					)	
-						
-	(options, args) = parser.parse_args()
-	
-	try:
-		options.maxChapterThreads = int(options.maxChapterThreads)
-	except:
-		options.maxChapterThreads = 2
-	
-	if (options.maxChapterThreads <= 0):
-		options.maxChapterThreads = 2;
-		
-	if(len(args) == 0 and ( not (options.convert_Directory or options.xmlfile_path != None) )):
-		parser.error('Manga not specified.')
-	
-	#if(len(args) > 1):
-	#	parser.error('Possible multiple mangas specified, please select one.  (Did you forget to put quotes around a multi-word manga?)')
-	
-	SetDownloadPathToName_Flag = False
-	SetOutputPathToDefault_Flag = False
-	if(len(args) > 0):
-		
-		# Default Directory is the ./MangaName
-		if (options.downloadPath == 'DEFAULT_VALUE'):
-			SetDownloadPathToName_Flag = True
+    parser.set_defaults(
+        all_chapters_FLAG=False,
+        auto=False,
+        conversion_FLAG=False,
+        convert_Directory=False,
+        device='Kindle 3',
+        downloadFormat='.cbz',
+        downloadPath='DEFAULT_VALUE',
+        inputDir=None,
+        outputDir='DEFAULT_VALUE',
+        overwrite_FLAG=False,
+        verbose_FLAG=False,
+        timeLogging_FLAG=False,
+        maxChapterThreads=3)
 
-			
-		# Default outputDir is the ./MangaName
-		if (options.outputDir == 'DEFAULT_VALUE'):
-			SetOutputPathToDefault_Flag = True
+    parser.add_option('--all',
+        action='store_true',
+        dest='all_chapters_FLAG',
+        help='Download all available chapters.')
+
+    parser.add_option('--convertDirectory',
+        action='store_true',
+        dest='convert_Directory',
+        help='Converts the image files stored in the directory specified by --inputDirectory. Stores the converted images in the directory specified by --outputDirectory')
+
+    parser.add_option('-c', '--convertFiles',
+        action='store_true',
+        dest='conversion_FLAG',
+        help='Converts downloaded files to a Format/Size acceptable to the device specified by the --device parameter.')
+
+    parser.add_option('--device',
+        dest='device',
+        help='Specifies the conversion device. Omitting this option default to %default.')
+
+    parser.add_option('-d', '--directory',
+        dest='downloadPath',
+        help='The destination download directory.  Defaults to the directory of the script.')
+
+    parser.add_option('--inputDirectory',
+        dest='inputDir',
+        help='The directory containing the images to convert when --convertDirectory is specified.')
+
+    parser.add_option('--outputDirectory',
+        dest='outputDir',
+        help='The directory to store the images when --convertDirectory is specified.')
+
+    parser.add_option('--overwrite',
+        action='store_true',
+        dest='overwrite_FLAG',
+        help='Overwrites previous copies of downloaded chapters.')
+
+    parser.add_option('-t', '--threads',
+        dest='maxChapterThreads',
+        help='Limits the number of chapter threads to the value specified.')
+
+    parser.add_option('--timeLogging',
+        action='store_true',
+        dest='timeLogging_FLAG',
+        help='Output time logging.')
+
+    parser.add_option('--verbose',
+        action='store_true',
+        dest='verbose_FLAG',
+        help='Verbose Output.')
+
+    parser.add_option('-x', '--xml',
+        dest='xmlfile_path',
+        help='Parses the .xml file and downloads all chapters newer than the last chapter downloaded for the listed mangas.')
+
+    parser.add_option('-z', '--zip',
+        action='store_const',
+        dest='downloadFormat',
+        const='.zip',
+        help='Downloads using .zip compression.  Omitting this option defaults to %default.')
+
+    (options, args) = parser.parse_args()
+
+    try:
+        options.maxChapterThreads = int(options.maxChapterThreads)
+    except:
+        options.maxChapterThreads = 2
+
+    if options.maxChapterThreads <= 0:
+        options.maxChapterThreads = 2
+
+if len(args) == 0 and ( not (options.convert_Directory or options.xmlfile_path is not None) ):
+    parser.error('Manga not specified.')
+
+SetDownloadPathToName_Flag = False
+SetOutputPathToDefault_Flag = False
+if len(args) > 0:
+    # Default Directory is the ./MangaName
+    if options.downloadPath == 'DEFAULT_VALUE':
+        SetDownloadPathToName_Flag = True
 
 
-	PILAvailable = isImageLibAvailable()
-	# Check if PIL Library is available if either of convert Flags are set 
-	if ((not PILAvailable)  and (options.convert_Directory or options.conversion_FLAG)):
-		print ("\nConversion Functionality Not available.\nMust install the PIL (Python Image Library)")
-		sys.exit()
-	else:
-		if (PILAvailable):
-			from ConvertPackage.ConvertFile import convertFile
-			
-	# Changes the working directory to the script location
-	if (os.path.dirname(sys.argv[0]) != ""):
-		os.chdir(os.path.dirname(sys.argv[0]))
+    # Default outputDir is the ./MangaName
+    if options.outputDir == 'DEFAULT_VALUE':
+        SetOutputPathToDefault_Flag = True
 
-	options.outputMgr = progressBarManager()
-	options.outputMgr.start()
-	try:
-		if (options.convert_Directory):
-			if ( options.outputDir == 'DEFAULT_VALUE' ):
-				options.outputDir = '.'
-			convertFile.convert(options.outputMgr, options.inputDir, options.outputDir, options.device, options.verbose_FLAG)
-	
-		elif options.xmlfile_path != None:
-			xmlParser = MangaXmlParser(options)
-			xmlParser.downloadManga()
-		else:
-			threadPool = []
-			for manga in args:
-				print( manga )
-				options.manga = manga
-			
-				if SetDownloadPathToName_Flag:		
-					options.downloadPath = ('./' + fixFormatting(options.manga))
-			
-				if SetOutputPathToDefault_Flag:	
-					options.outputDir = options.downloadPath 
-			
-				options.downloadPath = os.path.realpath(options.downloadPath) + os.sep
-				
-			
-				# site selection
-				print('\nWhich site?\n(1) MangaFox\n(2) OtakuWorks\n(3) MangaReader\n')
-	
-				# Python3 fix - removal of raw_input()
-				try:
-					site = raw_input()
-				except NameError:
-					site = input()
+PILAvailable = isImageLibAvailable()
 
-				try:
-					options.site = siteDict[site]
-				except KeyError:
-					raise InvalidSite('Site selection invalid.')	
-			
-				threadPool.append(SiteParserThread(options, None, None))
-			
-			for thread in threadPool: 
-				thread.start()
-				thread.join()
-	finally:
-		# Must always stop the manager
-		options.outputMgr.stop()
-		
-		
+# Check if PIL Library is available if either of convert Flags are set
+if (not PILAvailable) and (options.convert_Directory or options.conversion_FLAG):
+    print ("\nConversion Functionality Not available.\nMust install the PIL (Python Image Library)")
+    sys.exit()
+else:
+    if PILAvailable:
+        from ConvertPackage.ConvertFile import convertFile
+
+# Changes the working directory to the script location
+if os.path.dirname(sys.argv[0]) != "":
+    os.chdir(os.path.dirname(sys.argv[0]))
+
+options.outputMgr = progressBarManager()
+options.outputMgr.start()
+try:
+    if options.convert_Directory:
+        if options.outputDir == 'DEFAULT_VALUE':
+            options.outputDir = '.'
+        convertFile.convert(options.outputMgr, options.inputDir, options.outputDir, options.device,
+            options.verbose_FLAG)
+
+    elif options.xmlfile_path is not None:
+        xmlParser = MangaXmlParser(options)
+        xmlParser.downloadManga()
+    else:
+        threadPool = []
+        for manga in args:
+            print( manga )
+            options.manga = manga
+
+            if SetDownloadPathToName_Flag:
+                options.downloadPath = ('./' + fixFormatting(options.manga))
+
+            if SetOutputPathToDefault_Flag:
+                options.outputDir = options.downloadPath
+
+            options.downloadPath = os.path.realpath(options.downloadPath) + os.sep
+
+
+            # site selection
+            print('\nWhich site?\n(1) MangaFox\n(2) OtakuWorks\n(3) MangaReader\n')
+
+            # Python3 fix - removal of raw_input()
+            try:
+                site = raw_input()
+            except NameError:
+                site = input()
+
+            try:
+                options.site = siteDict[site]
+            except KeyError:
+                raise InvalidSite('Site selection invalid.')
+
+            threadPool.append(SiteParserThread(options, None, None))
+
+        for thread in threadPool:
+            thread.start()
+            thread.join()
+finally:
+    # Must always stop the manager
+    options.outputMgr.stop()
+
 if __name__ == '__main__':
-	main()
+    main()
